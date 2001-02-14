4c9d36f3e3bbc3aa7d9e3ce087858d61db2c16ec
sridhar <sridhar>
2001-02-14 15:35:38 +0000

    Added copyright and GPL notice

--- src/main/perl/lib/Bedrock/Apache/pdfrock.pl
+++ src/main/perl/lib/Bedrock/Apache/pdfrock.pl
@@ -3,6 +3,26 @@
 # ident "@(#) $Header$"
 # ident "@(#)        $Name$"
 
+#
+#    This file is a part of Bedrock, a server-side web scripting tool.
+#    Check out http://www.openbedrock.org
+#    Copyright (C) 2001, Charles Jones, LLC.
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+
 BEGIN
 {
     if ( open( FILE, 'perl.INC' ) ) {
@@ -146,6 +166,9 @@ if ( $@ ) {
 #
 # Name of Release: $Name$
 # $Log$
+# Revision 1.2  2001/02/14 15:35:43  sridhar
+# Added copyright and GPL notice
+#
 # Revision 1.1  2000/12/18 18:03:47  sridhar
 # 1. Moved Perl/apps/PDFrock/PDFrock.pl to Bedrock/Apache/pdfrock.pl
 # 2. Probably doesn't work yet
