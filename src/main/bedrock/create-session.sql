a9be74539699a7cf936d6b76dd61a5c0543c0428
eutl420 <eutl420>
2011-08-08 14:13:29 +0000

    - first check-in

---
+++ src/main/bedrock/create-session.sql
@@ -0,0 +1,20 @@
+grant all privileges on bedrock.* to fred@localhost identified by 'flintstone';
+
+drop table if exists session;
+
+create table session
+ (
+  id           int(11)      not null auto_increment primary key,
+  session      varchar(50)  not null default '',
+  login_cookie varchar(50)  not null default '',
+  username     varchar(50)  not null default '',
+  password     varchar(30)  default null,
+  firstname    varchar(30)  default null,
+  lastname     varchar(50)  default null,
+  email        varchar(100) default null,
+  prefs        text,
+  updated      timestamp    not null default current_timestamp on update current_timestamp,
+  added        datetime     default null,
+  expires      datetime     default null
+);
+
